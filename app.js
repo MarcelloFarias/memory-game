374670749bc4b976d317661a31e78599ff11d496
Marcello <marcellorocha632@gmail.com>
2022-11-25 11:07:39 -0300

    Creating a function that shuffle the array for generate random indexes

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ function generateCard(element) {
     return card;
 }
 
+function shuffleIndexes(array) {
+    for(let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
+
