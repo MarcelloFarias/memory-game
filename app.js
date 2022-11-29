3f99ab34e9c72f890203d278b81999395384bdc9
Marcello <marcellorocha632@gmail.com>
2022-11-29 08:47:14 -0300

    Fixing the verification of clicked cards

--- app.js
+++ app.js
@@ -15,11 +15,10 @@ function shuffleIndexes(array) {
 }
 
 function distributeElements() {
-    const indexesList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    const shuffledIndexesList = shuffleIndexes(indexesList);
+    const shuffledIndexesList = shuffleIndexes(elements);
 
-    for(let i = 0; i < elements.length; i++) {
-        emojiElement[i].innerText = elements[shuffledIndexesList[i]];
+    for(let i = 0; i < shuffledIndexesList.length; i++) {
+        emojiElement[i].innerText = shuffledIndexesList[i];
     }
 }
 
@@ -28,43 +27,34 @@ window.onload = () => {
 
     let selected = 0;
 
-    //flipper.forEach((element) => element.addEventListener('click', () => {
-        //if(selected < 2) {
-           // if(element.className == 'flipper') {
-                //element.classList.toggle('flipped');
-                
-                //selected++;
-            //}
-        //}
-    //}));
-
     for(let i = 0; i < flipper.length; i++) {
         flipper[i].addEventListener('click', () => {
             if(selected < 2) {
-                if(flipper[i].className === 'flipper' || flipper[i].className === 'different') {
+                if(flipper[i].className == 'flipper') {
                     flipper[i].classList.toggle('flipped');
                     if(selecteds.length < 2) {
                         selecteds.push({element: flipper[i], emoji: flipper[i].querySelector('.element').innerText});
                     }
                     if(selecteds.length == 2) {
                         if(selecteds[0].emoji == selecteds[1].emoji) {
-                            selecteds[0].element.classList.toggle('equals');
-                            selecteds[1].element.classList.toggle('equals');
-                            selecteds.splice(0, 1);
-                            selecteds.splice(0, 1);
-                            selected = 0;
+                            setTimeout(() => {
+                                selecteds[0].element.classList.toggle('equals');
+                                selecteds[1].element.classList.toggle('equals');
+                                selecteds.splice(0, 1);
+                                selecteds.splice(0, 1);
+                                selected = 0;
+                            }, 1000);
                         }
                         else {
                             setTimeout(() => {
-                                selecteds[0].element.classList.toggle('different');
-                                selecteds[1].element.classList.toggle('different');
+                                selecteds[0].element.classList.toggle('flipped');
+                                selecteds[1].element.classList.toggle('flipped');
                                 selecteds.splice(0, 1);
                                 selecteds.splice(0, 1);
                                 selected = 0;
                             }, 2000);
                         }
                     }
-                    console.log(selecteds)
                     selected++;
                 }
             }
